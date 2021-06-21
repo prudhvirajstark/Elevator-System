5c0d65c398f128915620ee5d43ee6b49201fd19c
prudhvirajstark <panisettiprudhviraj@gmail.com>
2021-06-21 18:10:24 +0200

    Added complete elevator system

--- include/elevator.h
+++ include/elevator.h
@@ -6,7 +6,7 @@
 #include <string>
 #include "request.h"
 
-
+class Elevator;
 
 class ExternalRequest;
 class InternalRequest;
@@ -18,16 +18,45 @@ enum class Status
     kIdle =2
 };
 
-class ElevatorButton: public Elevator
+class ElevatorButton
 {
     private:
+        int level = 0;
+        Elevator* elevator = nullptr;
     public:
+        ElevatorButton(int l, Elevator* e): level(1), elevator(e){}
+
+        void pressButton();
 };
 
 class Elevator
 {
     private:
+        std::vector<ElevatorButton> buttons_;
+        std::vector<bool> up_stops_;
+        std::vector<bool> down_stops_;
+        int current_level_;
+        Status status_;
     public:
+        Elevator(int n);
+
+        void insertButton(ElevatorButton eb);
+        void handleExternalRequest(ExternalRequest r);
+        void handleInternalRequest(InternalRequest r);
+        void openGate();
+        void closeGate();
+        bool noRequests(const std::vector<bool>& stops);
+        std::string getElevatorRequestListInfo() const;
+        std::string getElevatorStatusInfo() const;
+        Status getStatus()
+        {
+            return status_;
+        };
+        int getCurLevel() const
+        {
+            return current_level_;
+        };
+
 };
 
 
